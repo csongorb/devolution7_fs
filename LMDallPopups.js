e0d5baaecc0894f06c5063ccb4d4f9d64bce2c3d
Sophie <sophiew0922@gmail.com>
2024-01-30 13:55:34 +0100

    copied PopUp Links

--- LMDallPopups.js
+++ LMDallPopups.js
@@ -1,9 +1,219 @@
 
-const megagon_industries_sw = L.latLng(81.16875812380145, -55.95336914062501);
-const megagon_industries_ne = L.latLng(82.04665216250261, -49.010009765625);
-const bounds = [megagon_industries_sw, megagon_industries_ne];
-const rectangle = L.rectangle(bounds, {
+var megagon_industries_sw = L.latLng(81.16875812380145, -55.95336914062501);
+var megagon_industries_ne = L.latLng(82.04665216250261, -49.010009765625);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
   color: "#66000000"
 })
 .bindPopup(`<a href=https://megagonindustries.com/ target="_blank" rel="noreferrer">Megagon Industries Website</a>`)
 .addTo(map);
+
+
+var megagon_industries_sw = L.latLng(70.34092679475283, -86.94305419921875);
+var megagon_industries_ne = L.latLng(71.19837924340516, -84.94354248046876);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.pcgamer.com/lonely-mountains-downhill-is-the-mountain-biking-game-i-never-knew-i-needed/ target="_blank" rel="noreferrer">PC Gamer Interview</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(70.34092679475283, -84.83093261718751);
+var megagon_industries_ne = L.latLng(71.18421085713157, -82.56774902343751);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://dirtmountainbike.com/videos/watch-lonely-mountains-new-downhill-mtb-video-game-trailer target="_blank" rel="noreferrer">Greenlight Trailer</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(2.3065056838291094, -115.21362304687501);
+var megagon_industries_ne = L.latLng(5.419148251825298, -110.86303710937501);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.reddit.com/r/xboxone/comments/dmc4d3/lonely_mountains_downhill_is_is_out_now_on_game/ target="_blank" rel="noreferrer">Reddit Game Pass Thread</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(-10.568822151626943, -90.53009033203125);
+var megagon_industries_ne = L.latLng(-7.819847426192575, -88.45367431640625);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://lonelymountains.com/lonely-mountains/change-log/ target="_blank" rel="noreferrer">LMD Change Log</a>`)
+.addTo(map);
+
+
+
+
+var megagon_industries_sw = L.latLng(-18.041421221891937, -90.28015136718751);
+var megagon_industries_ne = L.latLng(-14.477234210156519, -87.7093505859375);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://80.lv/articles/level-game-production-lonely-mountains-downhill/ target="_blank" rel="noreferrer">80LV Interview</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(-8.602747284770018, -83.76525878906251);
+var megagon_industries_ne = L.latLng(-4.850154078505659, -81.3702392578125);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.gamedeveloper.com/disciplines/road-to-the-igf-megagon-industries-i-lonely-mountains-downhill-i- target="_blank" rel="noreferrer">Game Developer Interview</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(69.34604503609113, -79.74700927734376);
+var megagon_industries_ne = L.latLng(70.14465591204957, -76.65985107421875);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/_-KESI3qIBg- target="_blank" rel="noreferrer">Greenlight Trailer</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(81.70917918992257, -125.10955810546876);
+var megagon_industries_ne = L.latLng(82.30837788909356, -119.36920166015626);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/ojY8gIuQ8VI- target="_blank" rel="noreferrer">Yeti Mountain</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(81.68720912699644, -130.42419433593753);
+var megagon_industries_ne = L.latLng(82.17138585132723, -125.86486816406251);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/Ox-9IOBbJnM- target="_blank" rel="noreferrer">Lara Croft</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(81.28030141150958, -48.67218017578126);
+var megagon_industries_ne = L.latLng(81.68855913146949, -44.98077392578126);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/R7g7UlFlMA8- target="_blank" rel="noreferrer">and then it rained</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(81.27696971088555, -44.76654052734375);
+var megagon_industries_ne = L.latLng(81.68697086546005, -41.05865478515626);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/x97HDeZO5ME- target="_blank" rel="noreferrer">twisted lines trailer</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(68.75490274229087, -87.58026123046875);
+var megagon_industries_ne = L.latLng(69.73580786129756, -84.38873291015626);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/sc5Wynoihs0- target="_blank" rel="noreferrer">lmd climate change edition</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(67.25781893884714, -102.92266845703126);
+var megagon_industries_ne = L.latLng(68.1394652752747, -99.82452392578126);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/Dj8MOIxOdUY- target="_blank" rel="noreferrer">lmd kickstarter trailer</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(63.96221988247847, -85.64666748046876);
+var megagon_industries_ne = L.latLng(65.14680748791784, -81.95526123046876);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/e0qrvuNWofk- target="_blank" rel="noreferrer">lmd demo gameplay</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(8.323473517578963, -96.44348144531251);
+var megagon_industries_ne = L.latLng(12.793588915696942, -90.35705566406251);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/nfJxYa3kt5U- target="_blank" rel="noreferrer">lmd launch trailer</a>`)
+.addTo(map);
+
+
+
+var megagon_industries_sw = L.latLng(-14.799845265580686, -81.75750732421876);
+var megagon_industries_ne = L.latLng(-12.488604694020848, -78.60443115234376);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/qpM_C2eSm-0- target="_blank" rel="noreferrer">lmd dev speedrun</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(2.035338263850917, -64.26452636718751);
+var megagon_industries_ne = L.latLng(5.783245444138244, -59.2327880859375);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/RFCKVPaETUk- target="_blank" rel="noreferrer">lmd eldfjall island</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(-22.815175182710817, -51.02874755859376);
+var megagon_industries_ne = L.latLng(-21.093213073347744, -48.53485107421876);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/jiqFO3n6vJ4- target="_blank" rel="noreferrer">lmd rileys return</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(59.02642243660573, -66.06628417968751);
+var megagon_industries_ne = L.latLng(60.41656438628133, -62.35290527343751);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/rfRRMA3o-Zs- target="_blank" rel="noreferrer">lmd culling</a>`)
+.addTo(map);
+
+
+var megagon_industries_sw = L.latLng(-22.114650504047564, -39.64691162109376);
+var megagon_industries_ne = L.latLng(-21.088087883896886, -38.17474365234376);
+var bounds = [megagon_industries_sw, megagon_industries_ne];
+var rectangle = L.rectangle(bounds, {
+  color: "#66000000"
+})
+.bindPopup(`<a href=https://www.youtube.com/embed/IPbK53UtYL0- target="_blank" rel="noreferrer">lmd misty peak</a>`)
+.addTo(map);
+
