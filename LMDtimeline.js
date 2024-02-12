2c1dc47cda5c16e8c8dd6ae2a63f7f794995ab23
csongorb <csongorb@gmail.com>
2024-02-12 16:56:39 +0100

    Added tool to show exact map-positions on mouse-click.

--- LMDtimeline.js
+++ LMDtimeline.js
@@ -1,7 +1,7 @@
 var map = L.map('map');
 map.setView([72.59334083012024, -90.52734375], 3);
 
-L.tileLayer('https://csongorb.github.io/devolution7_fs/leaflet/CustomTiles/{z}/{x}/{y}.png', {
+L.tileLayer('leaflet/CustomTiles/{z}/{x}/{y}.png', {
     mapExtent: [0.00000000, -39288.00000000, 33000.00000000, 0.00000000],
     maxZoom: 8,
     minZoom: 2,
@@ -9,12 +9,17 @@ L.tileLayer('https://csongorb.github.io/devolution7_fs/leaflet/CustomTiles/{z}/{
     mapMinResolution: Math.pow(2, 8) * 0.50000000,
     noWrap: true,
     tileExtent: [0.00000000, -39288.00000000, 33000.00000000, 0.00000000],
-    attribution: 'Map data &copy; Devolution'
+    attribution: '&copy; Devolution'
 }).addTo(map);
 
 // close sidebar
 map.on('click', function() {
     sidebar.close();
 } );
+
+// tool: show location of mouse-click
+//map.on('click', function(ev) {
+//    alert(ev.latlng);
+//});
 
 var sidebar = L.control.sidebar('sidebar').addTo(map);
