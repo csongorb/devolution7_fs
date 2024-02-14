7d33f9439b3dc451e70527b963026c1d74b1dbb0
csongorb <csongorb@gmail.com>
2024-02-14 16:47:10 +0100

    Added all version to the timeline.

--- LMDtimelineVersions.js
+++ LMDtimelineVersions.js
@@ -2,12 +2,14 @@
 
 // 001.apk
 var markerAndroid001 = L.marker([80.30163, -88.69263])
-.bindPopup(`<h3 align="center">Android 003</h3>
-            <div align="center">
-              <small>Polished Prototype</small><br />
-              October 19th 2015<br />
-              <img src="images/versions/android_003_small.jpg" width="150" height="150" alt="Android_001"></img><br />
-              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/151019_downhill_003.apk"><img src="images/platforms/androidicon.png" width="40" height="40" alt="Android"></img></a><br />
+.bindPopup(`<div align="center">
+              <big><b>downhill_001.apk</b></big><br />
+              July 16th 2015<br />
+              <small>First Prototype</small><br />
+              <img src="images/versions/android_001_small.png" width="150" height="150" alt="Android_001"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/150716_downhill_001.apk"><img src="images/platforms/androidicon.png" width="40" height="40" alt="Android"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
             </div>`)
 .on('click', function () {
     //sidebar.open('versions');
@@ -19,8 +21,8 @@ var markerAndroid003 = L.marker([76.91315, -108.11096])
               <big><b>downhill_003.apk</b></big><br />
               October 19th 2015<br />
               <small>Polished Prototype</small><br />
-              <br />
-              <small>Download & play:</small><br />
+              <img src="images/versions/android_003_small.jpg" width="150" height="150" alt="Android_003"></img><br />
+              <small>Download:</small><br />
               <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/151019_downhill_003.apk"><img src="images/platforms/androidicon.png" width="40" height="40" alt="Android"></img></a><br />
               <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
             </div>`)
@@ -30,51 +32,116 @@ var markerAndroid003 = L.marker([76.91315, -108.11096])
 
 // 005.apk
 var markerAndroid005 = L.marker([75.89479, -98.65723])
-.bindPopup("005.apk")
+.bindPopup(`<div align="center">
+              <big><b>downhill_005.apk</b></big><br />
+              January 22nd 2016<br />
+              <small>Controller Experimentation<br />
+              on Mobile</small><br />
+              <img src="images/versions/android_005_small.png" width="150" height="150" alt="Android_005"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/160122_downhill_005.apk"><img src="images/platforms/androidicon.png" width="40" height="40" alt="Android"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 // 013.apk
 var markerAndroid013 = L.marker([75.86396, -75.99243])
-.bindPopup("013.apk")
+.bindPopup(`<div align="center">
+              <big><b>downhill_013.apk</b></big><br />
+              June 29th 2016<br />
+              <small>First Mountains &<br />
+              Transition to PC</small><br />
+              <img src="images/versions/android_013_small.png" width="150" height="150" alt="Android_013"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/160629_downhill_013.apk"><img src="images/platforms/androidicon.png" width="40" height="40" alt="Android"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 // Proto March 16
 var markerMarch16 = L.marker([73.25363, -94.51538])
-.bindPopup("Proto March 16")
+.bindPopup(`<div align="center">
+              <big><b>Proto March 16</b></big><br />
+              March 23rd 2016<br />
+              <small>Controller Experimentation<br />
+              on PC</small><br />
+              <img src="images/versions/protoMarch16_small.png" width="150" height="150" alt="Proto March 16"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/160323_Proto_March16.zip"><img src="images/platforms/winicon.png" width="40" height="40" alt="Win"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 // Proto August 16
 var markerAugust16 = L.marker([72.61697, -73.63037])
-.bindPopup("Proto August 16")
+.bindPopup(`<div align="center">
+              <big><b>Proto August 16</b></big><br />
+              August 16th 2016<br />
+              <small>First Mountains<br />
+              & Transition from Mobile</small><br />
+              <img src="images/versions/protoAugust16_small.png" width="150" height="150" alt="Proto August 16"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/160323_Proto_August16.zip"><img src="images/platforms/winicon.png" width="40" height="40" alt="Win"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 // Backer Demo
 var markerBackerDemo = L.marker([61.7939, -113.25806])
-.bindPopup("Backer Demo")
+.bindPopup(`<div align="center">
+              <big><b>Backer Demo</b></big><br />
+              November 18th 2017<br />
+              <small>Backer Demo</small><br />
+              <img src="images/versions/backerdemo_small.jpg" width="150" height="150" alt="Backer Demo"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/171118_BackerDemo_v1.zip"><img src="images/platforms/winicon.png" width="40" height="40" alt="Win"></img></a>
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/171118_BackerDemo_v1_MacOS.zip"><img src="images/platforms/osxicon.png" width="40" height="40" alt="OSX"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 // Dev Access 0.1.4
 var marker014 = L.marker([52.563, -55.17883])
-.bindPopup("Dev Access 0.1.4")
+.bindPopup(`<div align="center">
+              <big><b>Dev Access 0.1.4</b></big><br />
+              May 3rd 2018<br />
+              <small>New Terrain System<br />
+              & Growing Mountains</small><br />
+              <img src="images/versions/version_1_1_4_small.jpg" width="150" height="150" alt="Dev Access 0.1.4"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/180503_DevAccess_0_1_4.zip"><img src="images/platforms/winicon.png" width="40" height="40" alt="Win"></img></a>
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/180503_DevAccess_0_1_4_MacOS.zip"><img src="images/platforms/osxicon.png" width="40" height="40" alt="OSX"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 // Dev Access 0.1.7
 var marker017 = L.marker([42.13897, -84.4519])
-.bindPopup("Dev Access 0.1.7")
+.bindPopup(`<div align="center">
+              <big><b>Dev Access 0.1.7</b></big><br />
+              September 10th 2018<br />
+              <small>Progression System<br />
+              & More Mountains</small><br />
+              <img src="images/versions/version_1_1_7_small.jpg" width="150" height="150" alt="Dev Access 0.1.7"></img><br />
+              <small>Download:</small><br />
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/180910_DevAccess_0_1_7.zip"><img src="images/platforms/winicon.png" width="40" height="40" alt="Win"></img></a>
+              <a target="_blank" rel="noopener noreferrer" href="http://devolution.online/lmd/180910_DevAccess_0_1_7_MacOS.zip"><img src="images/platforms/osxicon.png" width="40" height="40" alt="OSX"></img></a><br />
+              <small><a onclick="sidebar.open('versions')" href="#">Disclaimer & more</a></small>
+            </div>`)
 .on('click', function () {
-    sidebar.open('versions');
+    //sidebar.open('versions');
 });
 
 var versionMarkers = L.layerGroup([markerAndroid001, markerAndroid003, markerAndroid005, markerAndroid013, markerMarch16, markerAugust16, markerBackerDemo, marker014, marker017])
