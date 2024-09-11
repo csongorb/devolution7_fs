e17061f8c3cd75bf03fb8c4028b831496c5e5864
csongorb <csongorb@gmail.com>
2024-09-11 21:16:40 +0200

    Exchanged "sidebar-v2" with "leaflet-sidebar-v2".

--- LMDtimeline.js
+++ LMDtimeline.js
@@ -29,4 +29,10 @@ map.attributionControl.addAttribution(`<a onclick="sidebar.open('privacy')" href
 //    alert(ev.latlng);
 //});
 
-var sidebar = L.control.sidebar('sidebar').addTo(map);
+//var sidebar = L.control.sidebar('sidebar').addTo(map);
+var sidebar = L.control.sidebar({
+    autopan: true,       // whether to maintain the centered map point when opening the sidebar
+    closeButton: true,    // whether t add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'left',     // left or right
+}).addTo(map);
